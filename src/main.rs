17d1c2458ec74f2168b5b2716f526a2a4d863ac6
racoonrocket <marnierg@gmail.com>
2025-07-01 00:18:07 +0200

    adjusted rate limit

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,8 @@ async fn main() {
         .allow_headers(Any);
 
     let governor_config = GovernorConfigBuilder::default()
-        .per_second(700)
-        .burst_size(170)
+        .per_second(1000)
+        .burst_size(400)
         .finish()
         .unwrap();
 
