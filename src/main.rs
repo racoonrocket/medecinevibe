ee00dd64428bebfebff8258c6c217cf74e2ea62e
racoonrocket <marnierg@gmail.com>
2025-12-01 22:04:13 +0100

    New dataset 2025

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     
-  /*  let addr = SocketAddr::from(([127, 0, 0, 1], port)); */
+    /*let addr = SocketAddr::from(([127, 0, 0, 1], port));*/
     println!("🌐 Serveur en écoute sur http://{}", addr);
 
     let listener = tokio::net::TcpListener::bind(&addr)
